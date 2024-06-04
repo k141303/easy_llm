9dacb94064037928cd32d3d55da34aba394b0643
k141303 <kouta.nakayama@gmail.com>
2024-06-04 22:34:39 +0900

    モデルの追加

--- src/easy_llm/llm.py
+++ src/easy_llm/llm.py
@@ -13,7 +13,7 @@ load_dotenv()
 
 class LLM(object):
     def __init__(self, cfg_path):
-        self.cfg = OmegaConf.load(CONFIG_PATH)
+        self.cfg = OmegaConf.load(cfg_path)
 
         self.tokenizer = AutoTokenizer.from_pretrained(
             **self.wrp_tokenizer_from_pretrained(**self.cfg.tokenizer.from_pretrained)
@@ -27,16 +27,22 @@ class LLM(object):
             self.model = AutoModelForCausalLM.from_pretrained(
                 **self.wrp_model_from_pretrained(**self.cfg.model.from_pretrained)
             )
+
+        print(
+            "Load: {}".format(
+                self.cfg.model.from_pretrained.pretrained_model_name_or_path
+            )
+        )
         self.model.eval()
 
     def wrp_tokenizer_from_pretrained(self, **kwargs):
         kwargs["token"] = os.environ["HF_TOKEN"]
-        kwargs["cache_dir"] = "./.cache"
+        kwargs["cache_dir"] = os.environ.get("HF_CACHE_DIR", "./.cache")
         return kwargs
 
     def wrp_model_from_pretrained(self, **kwargs):
         kwargs["token"] = os.environ["HF_TOKEN"]
-        kwargs["cache_dir"] = "./.cache"
+        kwargs["cache_dir"] = os.environ.get("HF_CACHE_DIR", "./.cache")
         if "torch_dtype" in kwargs:
             kwargs["torch_dtype"] = eval(kwargs["torch_dtype"])
         return kwargs
@@ -57,9 +63,14 @@ class LLM(object):
             ).to(self.model.device)
         else:
             prompt = self.cfg.prompt.format(user_content=user_content)
-            tokenized_input = self.tokenizer.encode(
-                prompt, **self.cfg.tokenizer.encode
-            ).to(self.model.device)
+            if "encode" in self.cfg.tokenizer:
+                tokenized_input = self.tokenizer.encode(
+                    prompt, **self.cfg.tokenizer.encode
+                ).to(self.model.device)
+            else:
+                tokenized_input = self.tokenizer(
+                    prompt, **self.cfg.tokenizer.call
+                ).input_ids.to(self.model.device)
 
         with torch.no_grad():
             output = self.model.generate(
@@ -69,15 +80,3 @@ class LLM(object):
 
         output = output[tokenized_input.size(-1) :]
         return self.tokenizer.decode(output, **self.cfg.tokenizer.decode)
-
-
-CONFIG_PATH = "config/model/Fugaku-LLM/Fugaku-LLM-13B-instruct.yaml"
-
-
-def main():
-    model = LLM(CONFIG_PATH)
-    print(model("自然言語処理とは何か"))
-
-
-if __name__ == "__main__":
-    main()
